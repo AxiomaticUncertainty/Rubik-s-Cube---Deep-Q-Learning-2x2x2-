7c2f459a5037bf240feb07bdd70170f63a2e73b7
AxiomaticUncertainty <axiomaticuncertainty@gmail.com>
2018-08-20 02:19:15 -0400

    Extended 'move' function in order to accommodate another side

--- rubiks_rl.py
+++ rubiks_rl.py
@@ -9,14 +9,13 @@ class cube:
 	def move(self, face, prime):
 		rep = copy.deepcopy(self.state)
 
+		# convoluted methodology by which we rotate a face of the cube
 		if face == 0:
 			if prime == True:
 				# rotated face
-				print(rep[0][0])
 				self.state[0][0] = rep[0][1]
 				self.state[0][1] = rep[0][2]
 				self.state[0][2] = rep[0][3]
-				print(rep[0][0])
 				self.state[0][3] = rep[0][0]
 				# right face (rel rot)
 				self.state[1][0] = rep[5][3]
@@ -48,6 +47,43 @@ class cube:
 				# bottom face (rel rot)
 				self.state[5][3] = rep[1][0]
 				self.state[5][2] = rep[1][3]
+		elif face == 1:
+			if prime == True:
+				# rotated face
+				self.state[1][0] = rep[1][1]
+				self.state[1][1] = rep[1][2]
+				self.state[1][2] = rep[1][3]
+				self.state[1][3] = rep[1][0]
+				# right face (rel rot)
+				self.state[2][0] = rep[5][0]
+				self.state[2][3] = rep[5][3]
+				# bottom face (rel rot)
+				self.state[5][0] = rep[0][2]
+				self.state[5][3] = rep[0][1]
+				# left face (rel rot)
+				self.state[0][2] = rep[4][2]
+				self.state[0][1] = rep[4][1]
+				# upper face (rel rot)
+				self.state[4][2] = rep[2][0]
+				self.state[4][1] = rep[2][3]
+			else:
+				# rotated face
+				self.state[1][0] = rep[1][3]
+				self.state[1][1] = rep[1][0]
+				self.state[1][2] = rep[1][1]
+				self.state[1][3] = rep[1][2]
+				# right face (rel rot)
+				self.state[2][0] = rep[4][2]
+				self.state[2][3] = rep[4][1]
+				# upper face (rel rot)
+				self.state[4][2] = rep[0][2]
+				self.state[4][1] = rep[0][1]
+				# left face (rel rot)
+				self.state[0][2] = rep[5][0]
+				self.state[0][1] = rep[5][3]
+				# bottom face (rel rot)
+				self.state[5][0] = rep[2][0]
+				self.state[5][3] = rep[2][3]
 
 	# for 'num' times, make random moves in order to scramble the cube
 	def scramble(self, num):
@@ -64,8 +100,3 @@ class nn:
 
 if __name__ == '__main__':
 	c = cube([[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15],[16,17,18,19],[20,21,22,23]]) # default arrangement, with each sub-list (face) being in the same order as is used for cube.move
-	print(c.state)
-	c.move(0, True)
-	print(c.state)
-	c.move(0, False)
-	print(c.state)
