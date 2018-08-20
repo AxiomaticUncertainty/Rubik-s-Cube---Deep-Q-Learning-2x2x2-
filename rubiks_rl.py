2115b4b66add9e33f41bb7c62b2969bc6ebb0494
AxiomaticUncertainty <axiomaticuncertainty@gmail.com>
2018-08-20 13:24:43 -0400

    Added all turn methods (12, in total)

--- rubiks_rl.py
+++ rubiks_rl.py
@@ -158,6 +158,80 @@ class cube:
 				# bottom face (rel rot)
 				self.state[5][2] = rep[0][0]
 				self.state[5][1] = rep[0][3]
+		elif face == 4:
+			if prime == True:
+				# rotated face
+				self.state[4][0] = rep[4][1]
+				self.state[4][1] = rep[4][2]
+				self.state[4][2] = rep[4][3]
+				self.state[4][3] = rep[4][0]
+				# right face (rel rot)
+				self.state[1][1] = rep[0][1]
+				self.state[1][0] = rep[0][0]
+				# bottom face (rel rot)
+				self.state[0][1] = rep[3][1]
+				self.state[0][0] = rep[3][0]
+				# left face (rel rot)
+				self.state[3][1] = rep[2][1]
+				self.state[3][0] = rep[2][0]
+				# upper face (rel rot)
+				self.state[2][1] = rep[1][1]
+				self.state[2][0] = rep[1][0]
+			else:
+				# rotated face
+				self.state[4][0] = rep[4][3]
+				self.state[4][1] = rep[4][0]
+				self.state[4][2] = rep[4][1]
+				self.state[4][3] = rep[4][2]
+				# right face (rel rot)
+				self.state[1][1] = rep[2][1]
+				self.state[1][0] = rep[2][0]
+				# upper face (rel rot)
+				self.state[2][1] = rep[3][1]
+				self.state[2][0] = rep[3][0]
+				# left face (rel rot)
+				self.state[3][1] = rep[0][1]
+				self.state[3][0] = rep[0][0]
+				# bottom face (rel rot)
+				self.state[0][1] = rep[1][1]
+				self.state[0][0] = rep[1][0]
+		elif face == 5:
+			if prime == True:
+				# rotated face
+				self.state[5][0] = rep[5][1]
+				self.state[5][1] = rep[5][2]
+				self.state[5][2] = rep[5][3]
+				self.state[5][3] = rep[5][0]
+				# right face (rel rot)
+				self.state[3][3] = rep[0][3]
+				self.state[3][2] = rep[0][2]
+				# bottom face (rel rot)
+				self.state[0][3] = rep[1][3]
+				self.state[0][2] = rep[1][2]
+				# left face (rel rot)
+				self.state[1][3] = rep[2][3]
+				self.state[1][2] = rep[2][2]
+				# upper face (rel rot)
+				self.state[2][3] = rep[3][3]
+				self.state[2][2] = rep[3][2]
+			else:
+				# rotated face
+				self.state[5][0] = rep[5][3]
+				self.state[5][1] = rep[5][0]
+				self.state[5][2] = rep[5][1]
+				self.state[5][3] = rep[5][2]
+				# right face (rel rot)
+				self.state[3][3] = rep[2][3]
+				self.state[3][2] = rep[2][2]
+				# upper face (rel rot)
+				self.state[2][3] = rep[1][3]
+				self.state[2][2] = rep[1][2]
+				# left face (rel rot)
+				self.state[1][3] = rep[0][3]
+				self.state[1][2] = rep[0][2]
+				# bottom face (rel rot)
+				self.state[0][3] = rep[3][3]
+				self.state[0][2] = rep[3][2]
 
 	# for 'num' times, make random moves in order to scramble the cube
 	def scramble(self, num):
